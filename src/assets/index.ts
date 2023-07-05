4448e13a059213be6d9927cd4d648a23a1456295
DenkyOgbonnaya <denkyogb@gmail.com>
2023-07-05 08:03:36 +0100

    fix:resolve file import path

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,7 +5,7 @@ export { default as Subscriptions } from "./icons/subscriptions.svg";
 export { default as Delete } from "./icons/delete.svg";
 export { default as Photo } from "./icons/add_a_photo.svg";
 export { default as Dashboard } from "./icons/dashboard.svg";
-export { default as Edit } from "./icons/Edit.svg";
+export { default as Edit } from "./icons/edit.svg";
 export { default as File } from "./icons/file_present.svg";
 export { default as Group } from "./icons/group.svg";
 export { default as Info } from "./icons/info.svg";
